4cf961d6db05cea9de621a6f9c4bd9c9f99adc54
Saul Rodriguez <saulrodriguez221299@gmail.com>
2026-05-20 19:09:47 -0600

    feat: initial commit

---
+++ src/heroes/pages/search/SearchPage.tsx
@@ -0,0 +1,13 @@
+import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+
+const SearchPage = () => {
+  return (<>
+    <CustomJumbotron 
+      title="Búsqueda de superheroes" 
+      description="Descubre, explora y administra tus superheroes y villanos favoritos."
+    />
+  </>
+  )
+}
+
+export default SearchPage;
