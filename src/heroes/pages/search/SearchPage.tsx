841de2a43737aaba5eeeb870800aef64c420814d
Saul Rodriguez <saulrodriguez221299@gmail.com>
2026-05-21 12:50:51 -0600

    feat(commonComponents): create hero cards related components

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,12 +1,21 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 const SearchPage = () => {
-  return (<>
-    <CustomJumbotron 
-      title="Búsqueda de superheroes" 
-      description="Descubre, explora y administra tus superheroes y villanos favoritos."
-    />
-  </>
+  return (
+    <>
+      <CustomJumbotron
+        title="Búsqueda de superheroes"
+        description="Descubre, explora y administra tus superheroes y villanos favoritos."
+      />
+      
+      {/* Stats Dahsboard */}
+      <HeroStats />
+
+      {/* Filter and search */}
+      <SearchControls />
+    </>
   )
 }
 
